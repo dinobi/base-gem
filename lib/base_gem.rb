153e7ce1a17ab47cc88497305e5f33126b2b6117
dinobi <dino.kennetcorp@gmail.com>
2019-06-04 23:49:43 +0100

    add test for base_gem

--- lib/base_gem.rb
+++ lib/base_gem.rb
@@ -1,5 +1,5 @@
-class BaseGem
+module BaseGem
   def self.welcome
-    puts 'Welcome to the start point for all your future gems ✌️'
+    'Welcome to the start point for all your future gems ✌️'
   end
 end
